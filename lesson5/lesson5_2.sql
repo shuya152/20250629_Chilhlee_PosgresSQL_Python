65bf71d499aabf8b39684afb0a7697c52c6927f3
shuya <shuya7711@gmail.com>
2025-07-13 03:35:09 +0000

    新增employee資料表的建立SQL語句及關聯式資料庫介紹

---
+++ lesson5/lesson5_2.sql
@@ -0,0 +1,13 @@
+-- DROP TABLE IF EXISTS employee CASCADE; --關聯式:連動刪除
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee(
+	emp_id SERIAL,
+	name VARCHAR(20),
+	birth_date DATE,
+	sex VARCHAR(1),
+	salary INT,
+	branch_id INT,
+	sup_id INT,
+ 	PRIMARY KEY(emp_id)
+);
